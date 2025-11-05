e40fb42186bbc82ea99847f39514b34ce541bc29
lftayna <limatayna115@gmail.com>
2025-11-04 21:23:05 -0300

    atividade da lapiseira

--- myrep/poo/lapiseira/py/draft.py
+++ myrep/poo/lapiseira/py/draft.py
@@ -41,35 +41,29 @@ class Lapiseira:
             return True
 
     def inserir(self, grafite: Grafite) -> bool:
-        if self.__ponta != None:
-            print("fail: ja existe grafite")
-            return False
         if grafite.get_espessura() != self.__espessura:
             print("fail: calibre incompatível")
             return False
-        self.__tambor.append(grafite)
-        self.__ponta = grafite
+        self.__tambor.append(grafite) 
         return True
 
     def puxar(self) -> bool:
         if self.__ponta is not None:
-            print("fail: já existe grafite no bico") 
+            print("fail: ja existe grafite no bico") 
             return False
         if len(self.__tambor) == 0:
             return False ##nao há tambor
         self.__ponta = self.__tambor.pop(0) ##removendo 
         return True
-            
-        
+               
     def remover(self) -> Grafite | None:
         aux = self.__ponta
         self.__ponta = None
         return aux
 
-
     def escreverPagina(self) -> None:
         if self.__ponta == None:
-            print("fail: nao existe grafite")
+            print("fail: nao existe grafite no bico")
             return
 
         gasto = {"HB": 1, "2B": 2, "4B": 4, "6B": 6}[self.__ponta.get_dureza()]
@@ -86,15 +80,9 @@ class Lapiseira:
         self.__ponta.usoPorFolha()
 
     def __str__(self) -> str:
-        if self.__ponta == None:
-            ponta_str = "[]"
-        else:
-            ponta_str = str(self.__ponta)
-        if len(self.__tambor) == 0:
-            tambor_str = "<>"
-        else:
-            tambor_str = " ".join(f"<{str(x)}>" for x in self.__tambor)
-        return f"calibre: {self.__espessura}, bico: {ponta_str}, tambor: {tambor_str}"
+        ponta_str = "[]" if self.__ponta is None else str(self.__ponta)
+        tambor_str = "".join(str(x) for x in self.__tambor)
+        return f"calibre: {self.__espessura}, bico: {ponta_str}, tambor: <{tambor_str}>"
         
 
 def main():
