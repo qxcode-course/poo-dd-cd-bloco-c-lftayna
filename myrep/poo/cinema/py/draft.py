5c2f7b6ec1acd5c64d6031cd4a6ff7f0d03fb305
lftayna <limatayna115@gmail.com>
2025-11-19 23:44:50 -0300

    cinema refeita

--- myrep/poo/cinema/py/draft.py
+++ myrep/poo/cinema/py/draft.py
@@ -1,31 +1,33 @@
 class Client:
-    def __init__(self, id: str, phone: int):
+    def __init__(self, id : str, phone : int):
         self.__id : str = id
         self.__phone : int = phone
 
-    def get_phone(self) -> int:
-        return self.__phone
-
-    def get_id(self) -> str:
+    def getId(self) -> str:
         return self.__id
 
-    def set_phone(self, fone: int):
-        self.__phone = fone
+    def getIdPhone(self) -> int:
+        return self.__phone
 
-    def set_id(self, id: str):
-        self.__id = id 
+    def setId(self, id : str):
+        self.__id = id
+
+    def setPhone(self, fone: int):
+        self.__phone = fone
 
     def __str__(self) -> str:
         return f"{self.__id}:{self.__phone}"
 
-class Theater:
+class Cinema:
     def __init__(self, capacity: int):
-        self.__seats: list[ Client | None] = [None] * capacity
+        self.__seats: list[Client | None] = [None] * capacity
         self.__capacity: int = capacity
 
     def get_seats(self) -> Client | None:
         return self.__seats
-
+    def __str__(self) -> str:
+        return f"[" + " ".join(str(x) if x is not None else "-" for x in self.__seats) + "]"
+        
     def reserve(self, id: str, phone: int, index: int) -> bool:
         if index < 0 or index >= len(self.__seats):
             print("fail: cadeira nao existe")
@@ -34,52 +36,40 @@ class Theater:
             print("fail: cadeira ja esta ocupada")
             return False
         for x in self.__seats:
-            if x is not None and x.get_id() == id:
+            if x is not None and x.getId() == id:
                 print("fail: cliente ja esta no cinema")
                 return False
-        self.__seats[index] = Client(id, phone)
-        return True
+            self.__seats[index] = Client(id, phone)
+            return True
 
     def cancel(self, id : str):
         for i in range(len(self.__seats)):
             client = self.__seats[i]
-            if client is not None and client.get_id() == id:
+            if client is not None and client.getId() == id:
                 self.__seats[i] = None
                 return
         print("fail: cliente nao esta no cinema")
-                
-
-
-    #def __search(self, name: str) -> int:
-    
-    #def __verifyIndix(self, index: int) -> bool: 
-
-    def __str__(self) -> str:
-         return f"[" + " ".join(str(x) if x is not None else "-" for x in self.__seats) + "]"
-        
 
 def main():
-    theater = Theater(0)
+    cinema = Cinema(0)
     while True:
-        line : str = input()
+        line: str = input()
         print("$" + line)
         args: list[str] = line.split(" ")
         if args[0] == "end":
             break
         if args[0] == "show":
-            print(theater)
+            print(cinema)
         if args[0] == "init":
             capacity = int(args[1])
-            theater = Theater(capacity)
+            cinema = Cinema(capacity)
         if args[0] == "reserve":
             id = args[1]
             phone = int(args[2])
             index = int(args[3])
-            theater.reserve(id, phone, index)
+            cinema.reserve(id, phone, index)
         if args[0] == "cancel":
             id = args[1]
-            theater.cancel(id)
+            cinema.cancel(id)
 
 main()
-
-    
